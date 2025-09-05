bca21a80cf88db7b6e74ac723674b19b47d539fc
Nikhil M Acharya <154912600+NIKHILMACHARYA@users.noreply.github.com>
2025-09-05 09:40:33 +0530

    Update Floyds triamgle.c

--- Floyds triamgle.c
+++ Floyds triamgle.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main()
 {
-          int i,j,n,count=0;
-          printf("Enter the rows:");
-          scanf("%d",&n);
-          for(i=1;i<=n;i++)
-          {
-                    for(j=1;j<=i;j++)
-                    {
-                              count++;
-                              printf("%d\t",count);
-                    }
-                    printf("\n");
-          }
-          return 0;
+    int i,j,n,count=0;
+    printf("Enter the rows:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+           for(j=1;j<=i;j++){
+                count++;
+                printf("%d\t",count);
+           }
+           printf("\n");
+     }
+     return 0;
 }
+
